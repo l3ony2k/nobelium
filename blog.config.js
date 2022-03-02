b7ddedfedb7a5a599b6092cb0caca2d0ede21e11
Leon <58762081+LeonWong0609@users.noreply.github.com>
2022-03-03 06:05:09 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -11,7 +11,7 @@ const BLOG = {
   darkBackground: '#000000', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2020, // If leave this empty, current year will be used.
-  postsPerPage: 8,
+  postsPerPage: 10,
   sortByDate: false,
   showAbout: true,
   showArchive: true,
