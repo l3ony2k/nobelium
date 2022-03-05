711f8322dad54c9e724f9e176e5d1e4295bb793d
Leon <wonderboy0609@outlook.com>
2022-03-06 01:48:10 +0800

    Revert "Update tailwind.config.js"
    This reverts commit e9c37c4c46420a8f90d2254102185ec8f1a73304.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 const BLOG = require('./blog.config')
 const { fontFamily } = require('tailwindcss/defaultTheme')
-const defaultTheme = require('tailwindcss/defaultTheme')
 const CJK = require('./lib/cjk')
 const fontSansCJK = !CJK()
   ? []
@@ -14,36 +13,6 @@ module.exports = {
   purge: ['./pages/**/*.js', './components/**/*.js', './layouts/**/*.js'],
   darkMode: BLOG.appearance === 'auto' ? 'media' : 'class', // or 'media' or 'class'
   theme: {
-    colors: ({ colors }) => ({
-      inherit: colors.inherit,
-      current: colors.current,
-      transparent: colors.transparent,
-      black: colors.black,
-      white: colors.white,
-      slate: colors.slate,
-      gray: colors.gray,
-      zinc: colors.zinc,
-      neutral: colors.neutral,
-      stone: colors.stone,
-      red: colors.red,
-      orange: colors.orange,
-      amber: colors.amber,
-      yellow: colors.yellow,
-      lime: colors.lime,
-      green: colors.green,
-      emerald: colors.emerald,
-      teal: colors.teal,
-      cyan: colors.cyan,
-      sky: colors.sky,
-      blue: colors.blue,
-      indigo: colors.indigo,
-      violet: colors.violet,
-      purple: colors.purple,
-      fuchsia: colors.fuchsia,
-      pink: colors.pink,
-      rose: colors.rose,
-    }),
-    fill: ({ theme }) => theme('colors'),
     extend: {
       colors: {
         day: {
@@ -51,18 +20,6 @@ module.exports = {
         },
         night: {
           DEFAULT: BLOG.darkBackground || '#111827'
-        },
-        'neutral': {
-          50:  '#fafafa',
-          100: '#f5f5f5',
-          200: '#e5e5e5',
-          300: '#d4d4d4',
-          400: '#a3a3a3',
-          500: '#737373',
-          600: '#525252',
-          700: '#404040',
-          800: '#262626',
-          900: '#171717',
         }
       },
       fontFamily: {
