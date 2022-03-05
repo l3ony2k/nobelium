529d64e226c4dbaae1d38eb1e65f72ac94b3754b
Leon <wonderboy0609@outlook.com>
2022-03-06 02:21:34 +0800

    default 000 & border

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
           DEFAULT: BLOG.lightBackground || '#ffffff'
         },
         night: {
-          DEFAULT: BLOG.darkBackground || '#111827'
+          DEFAULT: BLOG.darkBackground || '#000000'
         }
       },
       fontFamily: {
