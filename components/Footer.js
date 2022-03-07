1e889733a96ed91f2dc047c41763c0f4d10bea7e
Leon <wonderboy0609@outlook.com>
2022-03-08 03:01:34 +0800

    cc test update 2

--- components/Footer.js
+++ components/Footer.js
@@ -1,28 +1,27 @@
-import BLOG from '@/blog.config'
-import Vercel from '@/components/Vercel'
-import CC from '@/components/cc'
+import BLOG from "@/blog.config";
+//import Vercel from '@/components/Vercel'
+import CC from "@/components/cc";
 const Footer = ({ fullWidth }) => {
-  const d = new Date()
-  const y = d.getFullYear()
-  const from = +BLOG.since
+  const d = new Date();
+  const y = d.getFullYear();
+  const from = +BLOG.since;
   return (
     <div
       className={`mt-6 flex-shrink-0 m-auto w-full text-gray-600 dark:text-gray-500 transition-all ${
-        !fullWidth ? 'max-w-2xl px-4' : 'px-4 md:px-24'
+        !fullWidth ? "max-w-2xl px-4" : "px-4 md:px-24"
       }`}
     >
       <hr className="border-gray-200 border-opacity-50 dark:border-gray-600 dark:border-opacity-50" />
       <div className="mt-2 mb-4 leading-6">
         <div className="flex align-baseline justify-between flex-wrap items-center">
-          <p>
-            © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}
-          </p>
-          <Vercel />
           <CC />
+          <p>
+            {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}
+          </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
